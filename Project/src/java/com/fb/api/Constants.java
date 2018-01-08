9108fd3aa24dcd2d77f7358458b31ba2dcd99417
ShanthalakshmiM <shantha.aauma@gmail.com>
2018-01-08 23:08:46 +0530

    Update Constants.java

--- Project/src/java/com/fb/api/Constants.java
+++ Project/src/java/com/fb/api/Constants.java
@@ -11,11 +11,11 @@ package com.fb.api;
  */
 public class Constants {
     public static String MY_ACCESS_TOKEN = "";
-    public static final String APP_ID = "1248949948540245";
-    public static final String APP_SECRET = "96977b0709a53e4d06a8633a3efe03a5";
-    public static String PAGE_ACCESS_TOKEN = "EAACEdEose0cBAI6Clq0jA1x1yZCLc8ICWGrtCiQZCNbehc0jRv4X5u8GpHXePLtFhYEEZB93fw921QbyXJSnTYvDsS7eEg2a72kFm7FXahDnEV2DE2KuvQvYfwZB1NEuxA53gl1OoTNSbXmLtHN8139Y5Yqx0117Fyroy4pNmWy7D5xgtH2NIiQ4Cs6EQ5WlROruqYsjtgZDZD";
+    public static final String APP_ID = "XXXX";
+    public static final String APP_SECRET = "XXXX";
+    public static String PAGE_ACCESS_TOKEN = "XXXX";
     public static final String REDIRECT_URI = "http://localhost:8080/Project/login";
-    public static String PAGE_ID = "200864727141137";
+    public static String PAGE_ID = "XXXX";
     public static String recipient_id = null;
     public static String username = new String();
     public static String convoId = new String();
